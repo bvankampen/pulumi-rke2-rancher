6abc5935c8bda0e5b018e620daa183d09a64d6b4
Bas van Kampen <bas.vankampen@suse.com>
2023-11-10 14:16:40 +0100

    rename folder

--- files/run-install-rke2.sh
+++ files/run-install-rke2.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 RKE2_ACTIVE=$(systemctl is-active rke2-server)
 if [[ $RKE2_ACTIVE != "active" ]]; then
-  export INSTALL_RKE2_ARTIFACT_PATH=/opt/rke2/install/
-  sh /opt/rke2/install/install.sh
+  curl -sfL https://get.rke2.io | sh -
   systemctl enable rke2-server
   systemctl start rke2-server
 fi
